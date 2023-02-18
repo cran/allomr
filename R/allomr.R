246597b0138204e931b61702bad24c8b4f67adf9
Sämi Schär <saemi.schaer@gmail.com>
2023-02-18 07:50:08 +0000

    version 0.2.0

--- R/allomr.R
+++ R/allomr.R
@@ -1,9 +1,15 @@
-allomr <- function(X, Y){
+allomr <- function(X, Y, X0, a, b){
     m1 <- lm(log(Y)~log(X))
     n <- length(X[!is.na(X)])
-    a <- exp(m1$coefficients[1])
-    b <- m1$coefficients[2]
-    X0 <- mean(X, na.rm=TRUE)
+    if (missing(a)){
+      a <- exp(m1$coefficients[1])
+    }
+    if (missing(b)){
+      b <- m1$coefficients[2]
+    }
+    if (missing(X0)){
+      X0 <- mean(X, na.rm=TRUE)
+    }
     Yx <- Y*(X0/X)^b
     exp_e <- Y/(a*X^b)
     obj <-list(n, a, b, X0, Yx, exp_e)
